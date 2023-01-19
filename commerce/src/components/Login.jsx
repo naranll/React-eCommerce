d8a39c67b7fd303d22d9c697b50b10bcfc8bea2a
Morning <morning@admins-iMac-7.local>
2023-01-19 12:32:22 +0800

    login checking

--- commerce/src/components/Login.jsx
+++ commerce/src/components/Login.jsx
@@ -12,18 +12,24 @@ export default function Login() {
         if (users.map((user) => { user.username === name && user.password === pass })) {
             setLoginState(true);
             console.log("finally logged");
+        } else {
+            console.log("not");
         }
+        console.log("input", name, pass)
         console.log("login State at checkPoint:", loginState);
     }
 
     function getInput(input) {
         input.preventDefault();
-        setUserName(input.target.value);
-        setUserPass(input.target.value);
+        setUserName(input.target.inName.value);
+        setUserPass(input.target.inPass.value);
         checkUser(userName, userPass);
         console.log("login State at input:", loginState);
     }
 
+    function signUp() {
+        console.log("sign up btn");
+    }
 
     return <div className="loginPage">
         <div className="loginForm">
@@ -31,7 +37,7 @@ export default function Login() {
                 <input type="text" placeholder="Email or phone number" name="inName" />
                 <input type="text" placeholder="Password" name="inPass" />
                 <button type="submit">Sign In</button>
-                <input type="button" value="Sign Up" />
+                <input type="button" value="Sign Up" onClick={() => { signUp }} />
             </form>
         </div>
     </div>
